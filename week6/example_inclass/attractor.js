56efce65b95d01e49b26f08d18a441c10da87e48
Daniel Shiffman <daniel.shiffman@gmail.com>
2018-02-27 14:47:07 -0500

    Add files via upload

---
+++ week6/example_inclass/attractor.js
@@ -0,0 +1,78 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// An object for a draggable attractive body in our world
+
+class Attractor {
+  constructor() {
+    this.position = createVector(width / 2, height / 2);
+    this.mass = 20;
+    this.G = 1;
+    this.dragOffset = createVector(0, 0);
+    this.dragging = false;
+    this.rollover = false;
+  }
+  calculateAttraction(m) {
+    // Calculate direction of force
+    let dx = this.position.x - m.body.position.x;
+    let dy = this.position.y - m.body.position.y;
+    let force = createVector(dx, dy);
+    // Distance between objects
+    let distance = force.mag();
+    // Limiting the distance to eliminate "extreme" results for very close or very far objects
+    distance = constrain(distance, 5, 25);
+    // Normalize vector (distance doesn't matter here, we just want this vector for direction)
+    force.normalize();
+    // Calculate gravitional force magnitude
+    let strength = (this.G * this.mass * 0.01) / (distance * distance);
+    // Get force vector --> magnitude * direction
+    force.mult(strength);
+    return Vector.create(force.x, force.y);
+  }
+
+  // Method to display
+  display() {
+    ellipseMode(CENTER);
+    strokeWeight(4);
+    stroke(0);
+    if (this.dragging) {
+      fill(50);
+    } else if (this.rollover) {
+      fill(100);
+    } else {
+      fill(175, 200);
+    }
+    ellipse(this.position.x, this.position.y, this.mass * 2, this.mass * 2);
+  }
+
+  // The methods below are for mouse interaction
+  handlePress(mx, my) {
+    let d = dist(mx, my, this.position.x, this.position.y);
+    if (d < this.mass) {
+      this.dragging = true;
+      this.dragOffset.x = this.position.x - mx;
+      this.dragOffset.y = this.position.y - my;
+    }
+  }
+
+  handleHover(mx, my) {
+    let d = dist(mx, my, this.position.x, this.position.y);
+    if (d < this.mass) {
+      this.rollover = true;
+    } else {
+      this.rollover = false;
+    }
+  }
+
+  stopDragging() {
+    this.dragging = false;
+  }
+
+  handleDrag(mx, my) {
+    if (this.dragging) {
+      this.position.x = mx + this.dragOffset.x;
+      this.position.y = my + this.dragOffset.y;
+    }
+  }
+}
