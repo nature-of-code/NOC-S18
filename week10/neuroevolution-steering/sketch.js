045c558a66578b667ef28a324d201a18ee4b115d
Daniel Shiffman <daniel@shiffman.net>
2018-04-18 13:26:12 -0400

    cleanup and adjustments

--- week10/neuroevolution-steering/sketch.js
+++ week10/neuroevolution-steering/sketch.js
@@ -9,18 +9,18 @@ let population = [];
 
 // An array of "food"
 let food = [];
-// An array of "poison"
-let poison = [];
 
-// How good is food, how bad is poison?
-let nutrition = [0.5, -1];
-
-// Show additional info on DNA?
+// Show additional sensor info
 let debug;
 
 let speedSlider;
 let speedSpan;
 let best = null;
+
+let foodRadius = 4;
+let foodAmount = 50;
+let sensorLength = 75;
+
 
 function setup() {
 
@@ -35,67 +35,35 @@ function setup() {
   angleMode(RADIANS);
   for (let i = 0; i < 10; i++) {
     population[i] = new Vehicle(width / 2, height / 2);
-    //food[i] = createVector(width / 2 + 25, 0);
-  }
-
-  // Start with some food
-  for (let i = 0; i < 25; i++) {
-    food[i] = createVector(random(width), random(height));
-  }
-  // Start with some poison
-  for (let i = 0; i < 5; i++) {
-    //poison[i] = createVector(random(width), random(height));
   }
 }
 
-// Add new vehicles by dragging mouse
-// function mouseDragged() {
-//   population.push(new Vehicle(mouseX, mouseY));
-// }
-
 function draw() {
   background(0);
-
-  // food[0].x = mouseX;
-  // food[0].y = mouseY;
 
   let cycles = speedSlider.value();
   speedSpan.html(cycles);
 
   for (let n = 0; n < cycles; n++) {
-    // 10% chance of new food
-    if (random(1) < 0.1 || food.length < 25) {
+    // 10% chance of new food, always keep 25
+    while (food.length < 50) {
       food.push(createVector(random(50, width - 50), random(50, height - 50)));
-    }
-
-    // 1% chance of new poison
-    if (random(1) < 0.01) {
-      //poison.push(createVector(random(width), random(height)));
     }
 
     best = null;
     let record = -1;
-
     // Go through all vehicles
     for (let i = population.length - 1; i >= 0; i--) {
       let v = population[i];
-
       // Eat the food (index 0)
-      v.eat(food, 0);
-      // Eat the poison (index 1)
-      // v.eat(poison, 1);
-      // Check boundaries
-      // v.boundaries();
-      // v.wrap();
-
-      // Update and draw
-      v.update(food, poison);
-      // v.display();
-
+      v.eat(food);
+      v.think(food);
+      v.update(food);
       // If the vehicle has died, remove
       if (v.dead()) {
         population.splice(i, 1);
       } else {
+        // Is it the vehicles that has lived the longest?
         if (v.score > record) {
           record = v.score;
           best = v;
@@ -103,39 +71,31 @@ function draw() {
       }
     }
 
+    // If there is less than 20 apply reproduction
     if (population.length < 20) {
       for (let v of population) {
-        //Every vehicle has a chance of cloning itself
+        // Every vehicle has a chance of cloning itself according to score
         let child = v.birth(0.1 * v.score / record);
         if (child != null) {
           population.push(child);
         }
       }
     }
-
-    if (population.length < 3) {
-      let child = best.birth(1);
-      population.push(child);
-    }
   }
 
 
   // Draw all the food and all the poison
   for (let i = 0; i < food.length; i++) {
-    fill(0, 255, 0, 200);
-    noStroke();
-    ellipse(food[i].x, food[i].y, eat_threshold * 2);
+    fill(100, 255, 100, 200);
+    stroke(100, 255, 100);
+    ellipse(food[i].x, food[i].y, foodRadius * 2);
   }
 
-  for (let i = 0; i < poison.length; i++) {
-    fill(255, 0, 0);
-    noStroke();
-    ellipse(poison[i].x, poison[i].y, 4);
+  if (debug.checked()) {
+    best.highlight();
   }
 
-  best.highlight();
-  for (let i = population.length - 1; i >= 0; i--) {
-    let v = population[i];
+  for (let v of population) {
     v.display();
   }
 }
