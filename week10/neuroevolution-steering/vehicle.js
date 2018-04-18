632c5ae05cbc13c01b284cdad16ebebd81c8d117
Daniel Shiffman <daniel@shiffman.net>
2018-04-18 18:27:59 -0400

    Live Stream Changes
    Dear Diary,
    
    Today on The Coding Train I tried something new. Instead of making
    a tutorial, I just pulled up an example I'd been working on for
    quite some time and I added comments to it. Then I became obsessed
    with tuning it. But I really should go home and have dinner now
    because it's late. Maybe tomorrow the pull request fairy will
    visit me with some nice improvements.
    
    Mind you, I don't want to make the code more complex by improvements
    I really mean simplifications or small changes that improve the
    evolutionary algorithm or neural network model.
    
    I hope this example inspires some interesting work. Special thanks
    to @meiamsome for their contributions and also to @dariusk for
    inspiring me think of git commits as a diary.
    
    Sincerely yours,
    Daniel Sheeeeeefmahhhhn

--- week10/neuroevolution-steering/vehicle.js
+++ week10/neuroevolution-steering/vehicle.js
@@ -1,70 +1,86 @@
 // Daniel Shiffman
-// Nature of Code: Intelligence and Learning
-// https://github.com/shiffman/NOC-S17-2-Intelligence-Learning
+// Nature of Code 2018
+// https://github.com/shiffman/NOC-S18
 
 // Evolutionary "Steering Behavior" Simulation
 
-
+// This is a class for an individual sensor
+// Each vehicle will have N sensors
 class Sensor {
-  constructor(vec) {
-    this.vec = vec;
+  constructor(angle) {
+    // The vector describes the sensor's direction
+    this.dir = p5.Vector.fromAngle(angle);
+    // This is the sensor's reading
     this.val = 0;
   }
 }
 
-// Create a new vehicle
+// This is the class for each Vehicle
 class Vehicle {
-  constructor(x, y, brain) {
+  // A vehicle can be from a "brain" (Neural Network)
+  constructor(brain) {
 
     // All the physics stuff
     this.acceleration = createVector();
-    this.velocity = p5.Vector.random2D();
-    this.position = createVector(x, y);
+    this.velocity = createVector();
+    this.position = createVector(random(width), random(height));
     this.r = 4;
     this.maxforce = 0.1;
     this.maxspeed = 4;
     this.minspeed = 0.25;
-    this.velocity.setMag(this.maxspeed);
+    this.maxhealth = 3;
+
+    // This indicates how well it is doing
     this.score = 0;
 
-    this.sensorAngle = TWO_PI / totalSensors;
+    // Create an array of sensors
     this.sensors = [];
-    for (let angle = 0; angle < TWO_PI; angle += this.sensorAngle) {
-      this.sensors.push(new Sensor(p5.Vector.fromAngle(angle)));
+    for (let angle = 0; angle < TWO_PI; angle += sensorAngle) {
+      this.sensors.push(new Sensor(angle));
     }
 
-    if (arguments[2] instanceof NeuralNetwork) {
+    // If a brain is passed via constructor copy it
+    if (brain) {
       this.brain = brain.copy();
       this.brain.mutate(0.1);
+      // Otherwise make a new brain
     } else {
+      // inputs are all the sensors plus position and velocity info
       let inputs = this.sensors.length + 6;
+      // Arbitrary hidden layer
+      // 2 outputs for x and y desired velocity
       this.brain = new NeuralNetwork(inputs, 32, 2);
     }
 
-    // Health
+    // Health keeps vehicl alive
     this.health = 1;
   }
 
 
-  // Method to update location
+  // Called each time step
   update() {
     // Update velocity
     this.velocity.add(this.acceleration);
-    // Limit speed
+    // Limit speed to max
     this.velocity.limit(this.maxspeed);
+    // Keep speed at a minimum
     if (this.velocity.mag() < this.minspeed) {
       this.velocity.setMag(this.minspeed);
     }
+    // Update position
     this.position.add(this.velocity);
     // Reset acceleration to 0 each cycle
     this.acceleration.mult(0);
 
-    // Slowly die unless you eat
+    // Decrease health
+    this.health = constrain(this.health, 0, this.maxhealth);
     this.health -= 0.005;
+    // Increase score
     this.score += 1;
   }
 
   // Return true if health is less than zero
+  // or if vehicle leaves the canvas
   dead() {
     return (this.health < 0 ||
       this.position.x > width + this.r ||
@@ -74,42 +90,43 @@ class Vehicle {
     );
   }
 
-  // Small chance of returning a new child vehicle
-  birth(prob) {
+  // Make a copy of this vehicle according to probability
+  clone(prob) {
+    // Pick a random number
     let r = random(1);
     if (r < prob) {
-      // Same location, same DNA
-      //return new Vehicle(this.position.x, this.position.y, this.brain);
-      return new Vehicle(random(width), random(height), this.brain);
+      // New vehicle with brain copy
+      return new Vehicle(this.brain);
     }
+    // otherwise will return undefined
   }
 
   // Function to calculate all sensor readings
   // And predict a "desired velocity"
-  think(list) {
+  think(food) {
     // All sensors start with maximum length
     for (let j = 0; j < this.sensors.length; j++) {
       this.sensors[j].val = sensorLength;
     }
 
-    for (let i = 0; i < list.length; i++) {
+    for (let i = 0; i < food.length; i++) {
       // Where is the food
-      let otherPosition = list[i];
+      let otherPosition = food[i];
       // How far away?
       let dist = p5.Vector.dist(this.position, otherPosition);
-      // Skip if it's close
+      // Skip if it's too far away
       if (dist > sensorLength) {
         continue;
       }
 
-      // What's the angle of the food
+      // What is vector pointint to food
       let toFood = p5.Vector.sub(otherPosition, this.position);
 
       // Check all the sensors
       for (let j = 0; j < this.sensors.length; j++) {
         // If the relative angle of the food is in between the range
-        let delta = this.sensors[j].vec.angleBetween(toFood);
-        if (delta < this.sensorAngle / 2) {
+        let delta = this.sensors[j].dir.angleBetween(toFood);
+        if (delta < sensorAngle / 2) {
           // Sensor value is the closest food
           this.sensors[j].val = min(this.sensors[j].val, dist);
         }
@@ -118,15 +135,14 @@ class Vehicle {
 
     // Create inputs
     let inputs = [];
-    // Position in canvas
-    inputs[0] = constrain(map(this.position.x, 50, 0, 0, 1), 0, 1);
-    inputs[1] = constrain(map(this.position.y, 50, 0, 0, 1), 0, 1);
-    inputs[2] = constrain(map(this.position.x, width - 50, width, 0, 1), 0, 1);
-    inputs[3] = constrain(map(this.position.y, height - 50, height, 0, 1), 0, 1);
-    // Current velocity
+    // This is goofy but these 4 inputs are mapped to distance from edges
+    inputs[0] = constrain(map(this.position.x, foodBuffer, 0, 0, 1), 0, 1);
+    inputs[1] = constrain(map(this.position.y, foodBuffer, 0, 0, 1), 0, 1);
+    inputs[2] = constrain(map(this.position.x, width - foodBuffer, width, 0, 1), 0, 1);
+    inputs[3] = constrain(map(this.position.y, height - foodBuffer, height, 0, 1), 0, 1);
+    // These inputs are the current velocity vector
     inputs[4] = this.velocity.x / this.maxspeed;
     inputs[5] = this.velocity.y / this.maxspeed;
-
     // All the sensor readings
     for (let j = 0; j < this.sensors.length; j++) {
       inputs[j + 6] = map(this.sensors[j].val, 0, sensorLength, 1, 0);
@@ -137,19 +153,19 @@ class Vehicle {
     // Turn it into a desired velocity and apply steering formula
     let desired = createVector(2 * outputs[0] - 1, 2 * outputs[1] - 1);
     desired.mult(this.maxspeed);
+    // Craig Reynolds steering formula
     let steer = p5.Vector.sub(desired, this.velocity);
     steer.limit(this.maxforce);
+    // Apply the force
     this.applyForce(steer);
   }
 
-  // Check against array of food or poison
-  // index = 0 for food, index = 1 for poison
+  // Check against array of food
   eat(list) {
-    // Look at everything
     for (let i = list.length - 1; i >= 0; i--) {
       // Calculate distance
       let d = p5.Vector.dist(list[i], this.position);
-      // If we're withing 5 pixels, eat it!
+      // If vehicle is within food radius, eat it!
       if (d < foodRadius) {
         list.splice(i, 1);
         // Add health when it eats food
@@ -164,25 +180,27 @@ class Vehicle {
   }
 
   display() {
-
     // Color based on health
     let green = color(0, 255, 255, 255);
     let red = color(255, 0, 100, 100);
     let col = lerpColor(red, green, this.health)
 
     push();
+    // Translate to vehicle position
     translate(this.position.x, this.position.y);
-    // Extra info for sensors
+
+    // Draw lines for all the activated sensors
     if (debug.checked()) {
       for (let i = 0; i < this.sensors.length; i++) {
         let val = this.sensors[i].val;
         if (val > 0) {
           stroke(col);
           strokeWeight(map(val, 0, sensorLength, 4, 0));
-          let position = this.sensors[i].vec;
+          let position = this.sensors[i].dir;
           line(0, 0, position.x * val, position.y * val);
         }
       }
+      // Display score next to each vehicle
       noStroke();
       fill(255, 200);
       text(int(this.score), 10, 0);
@@ -202,6 +220,7 @@ class Vehicle {
     pop();
   }
 
+  // Highlight with a grey bubble
   highlight() {
     fill(255, 255, 255, 50);
     stroke(255);
